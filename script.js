9c109a83ef5296e3794899929753476c84e01dcf
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-06-18 14:39:50 +0000

    this is my css and js files

---
+++ script.js
@@ -0,0 +1,30 @@
+const ul = document.querySelector('ul'),
+const input = ul.querySelector('input');
+
+function createTag() {
+    tags.forEach(tag =>{
+        let liTag = <li>${tag} <i class="uit uit multiply"></i> </li>;
+        ul.insertAdjacentElement('afterbegin',liTag);
+    })
+}
+
+
+
+
+
+
+
+function addTag(e){
+    if(e.key == 'Enter'){
+        let tag = e.target.value.replace(/\s+/g, '   ');
+        if(tag.length > 1 && !tags.includes(tag)){
+            tag.split(',').forEach(tag => tags.push(tag));
+            createTag();
+        }
+    }
+}
+
+
+
+
+input.addEventListener('keyup', addTag);
