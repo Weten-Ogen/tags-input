cab6b78bcb856af2709546f902bdd649371edbfe
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-06-19 10:45:21 +0000

    adding my logics

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
-const ul = document.querySelector('ul'),
- input = ul.querySelector('input');
+const ul = document.querySelector("ul"),
+ input = ul.querySelector("input"),
+ countNumb = document.querySelector(".details span");
 
-function createTag() {
-    tags.forEach(tag =>{
-        let liTag = <li>${tag} <i class="uit uit multiply"></i> </li>;
-        ul.insertAdjacentElement('afterbegin',liTag);
-    })
+let maxTags = 10, tags = [];
+countTag();
+
+function countTag() {
+    countNumb.innerText = maxTags - tags.length;
 }
 
 
 
 
+function createTag() {
+    ul.querySelectorAll("li").forEach(li => li.remove());
+    tags.slice().reverse().forEach(tag =>{
+        let liTag = `<li>${tag} <i class="uit uit multiply" onclick="remove(this, '${tag}')"></i></li>`;
+        ul.insertAdjacentHTML("afterbegin",liTag);
+    });
+    countTag();
+}
+
+function remove(element, tag){
+    let index = tags.indexOf(tag);
+    tags =[...tags.slice(0,index), ...tags.slice(index + 1)];
+    element.parentElement.remove();
+    countTag();
+
+}
+
+
+
+ 
 
 
 
 function addTag(e){
-    if(e.key == 'Enter'){
-        let tag = e.target.value.replace(/\s+/g, '   ');
+    if(e.key == "Enter"){
+        let tag = e.target.value.replace(/\s+/g, ' ');
         if(tag.length > 1 && !tags.includes(tag)){
-            tag.split(',').forEach(tag => tags.push(tag));
-            createTag();
-        }
+           if(tags.length < 10){
+             tag.split(',').forEach(tag =>{
+                tags.push(tag);
+                createTag();
+              });
+            }
+         }
+        e.target.value = "";
     }
 }
 
 
 
 
-input.addEventListener('keyup', addTag);
+input.addEventListener("keyup", addTag);
+const removeBtn = document.querySelector("button");
+removeBtn.addEventListener("click", ()=>{
+    tags.length = 0;
+    ul.querySelectorAll("li").forEach(li => li.remove());
+    countTag();
+})
